4f68df7d9cef34c433cfb42307b1df8a19c38dfc
volvo007 <foxprowing@163.com>
2019-07-06 21:56:32 +0800

    import CSS

---
+++ README.md
@@ -0,0 +1,35 @@
+### python Dash exploration
+
+A set of scripts to explore the usage of python Dash.
+
+Right now, the script "uplod.py" is already archived. You can run this script under any python environment (python3 preferred).
+
+It will give an interface like below:
+
+![initial interface](./interface.jpg "Demo")
+
+You can then upload any Excel or CSV table (must have no empty rows at the beginnig) and you can choose any features you're interested then to check the scatter chart which generated automatically.
+
+The file "exploration.py", which focusing on flash_cache usage, is still under developing.
+
+----------
+
+Necessary libraries (latest version after 7/1/2019):
+
+numpy
+
+pandas
+
+plotly
+
+dash
+
+dash_core_components
+
+dash_core_html
+
+dash_auth
+
+dash_table
+
+flask_caching
---
+++ uplod.py
@@ -0,0 +1,248 @@
+import base64
+import datetime
+import io
+
+import dash
+import dash_auth
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+
+import pandas as pd
+import plotly.graph_objs as go
+
+# Keep this out of source code repository - save in a file or a database
+VALID_USERNAME_PASSWORD_PAIRS = {
+    'Jason': 'shell'
+}
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+auth = dash_auth.BasicAuth(
+    app,
+    VALID_USERNAME_PASSWORD_PAIRS
+)
+ 
+# available_indicators = df['Indicator Name'].unique()
+
+app.layout = html.Div([
+	# Upload component
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '90%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '2px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '20px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=False,
+    ),
+
+    # xaxis, need lower limitation or not, limitation value
+    html.Div([
+        dcc.Dropdown(
+            id='xaxis-column',
+            # options=options,
+            value='',
+            # value='ENGINE KM'
+        ),
+        dcc.RadioItems(
+            id='min-limit-value',
+            options=[{'label': i, 'value': i} for i in ['Lower-Limit', 'No Need']],
+            value='No Need',
+            labelStyle={'display': 'inline-block'}
+        ),
+        dcc.Input(
+        	id='min-lim-value',
+    		placeholder='Enter a value...',
+    		type='text',
+    		value=''
+		),
+    ],
+    style={'width': '48%', 'display': 'inline-block'}),
+
+    # yaxis, need upper limitation or not, limitation value
+    html.Div([
+        dcc.Dropdown(
+            id='yaxis-column',
+            # options=options,
+            value='',
+            # value='Iron ppm FE',
+            multi=True
+        ),
+        dcc.RadioItems(
+            id='upper-limit-value',
+            options=[{'label': i, 'value': i} for i in ['Upper-Limit', 'No Need']],
+            value='No Need',
+            labelStyle={'display': 'inline-block'}
+        ),
+        dcc.Input(
+        	id='max-lim-value',
+    		placeholder='Enter a value...',
+    		type='text',
+    		value=''
+		),
+    ],
+    style={'width': '48%', 'display': 'inline-block'}),
+
+
+    html.Div(id='output-data-upload'),
+])
+
+
+# get useful data from dataset
+def parse_contents(contents, xName, minLim, minLimVal, yNames, maxLim, maxLimVal, filename, states):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    # df_required = df.loc[:, nameList]
+    traces = []
+    # print(yNames)
+    # print(df.columns)
+    for i in yNames:
+    	traces.append(go.Scatter(
+    							x=df.loc[:, xName],
+    							y=df.loc[:, i],
+    							mode='markers',
+    							marker={'size': 12, 'opacity': 0.5,
+    									'line': {'width': 0.5, 'color': 'white'}},
+    							name=i,
+    		))
+
+    # 根据 minLim等的情况，决定是否加线
+    # plt.vlines(0, 0, 0.5, colors = "c", linestyles = "dashed")
+    # plotly 里面加线：https://plot.ly/python/shapes/
+    if (minLim == 'No Need') & (maxLim == 'No Need'):
+    	    layout = go.Layout(xaxis={'title': xName},
+    					margin={'l': 40, 'r':20, 't':20, 'b':30},
+    					hovermode='closest')
+    if (minLim != 'No Need') & (maxLim == 'No Need'):
+            layout = go.Layout(xaxis={'title': xName},
+    					margin={'l': 40, 'r':20, 't':20, 'b':30},
+    					hovermode='closest',
+                        shapes=[
+                            {'type': 'line', 'x0': min(df[xName])*0.9, 'y0': minLimVal, 
+                            'x1': max(df[xName])*1.1, 'y1': minLimVal,
+                            'line': {'color': 'rgb(240, 48, 48)', 'width':2, 'dash': 'dashdot'}}
+                        ])
+    if (minLim == 'No Need') & (maxLim != 'No Need'):
+            layout = go.Layout(xaxis={'title': xName},
+    					margin={'l': 40, 'r':20, 't':20, 'b':30},
+    					hovermode='closest',
+                        shapes=[
+                            {'type': 'line', 'x0': min(df[xName])*0.9, 'y0': maxLimVal, 
+                            'x1': max(df[xName])*1.1, 'y1': maxLimVal,
+                            'line': {'color': 'rgb(240, 48, 48)', 'width':2, 'dash': 'dashdot'}}
+                        ])
+    if (minLim != 'No Need') & (maxLim != 'No Need'):
+            layout = go.Layout(xaxis={'title': xName},
+    					margin={'l': 40, 'r':20, 't':20, 'b':30},
+    					hovermode='closest',
+                        shapes=[
+                            {'type': 'line', 'x0': min(df[xName])*0.9, 'y0': maxLimVal, 
+                            'x1': max(df[xName])*1.1, 'y1': maxLimVal,
+                            'line': {'color': 'rgb(240, 48, 48)', 'width':2, 'dash': 'dashdot'}},
+                            {'type': 'line', 'x0': min(df[xName])*0.9, 'y0': minLimVal, 
+                            'x1': max(df[xName])*1.1, 'y1': minLimVal,
+                            'line': {'color': 'rgb(240, 48, 48)', 'width':2, 'dash': 'dashdot'}
+                            }
+                        ])
+
+    return html.Div([
+        html.H5(filename),
+        html.H6(datetime.datetime.fromtimestamp(states)),
+	    dcc.Graph(
+	    	figure={
+	         'data': traces,
+	         'layout': layout,}
+	    	)
+    ])
+
+@app.callback(Output('output-data-upload', 'children'),
+			 [Input('upload-data', 'contents'),
+			  Input('xaxis-column', 'value'),
+			  Input('min-limit-value', 'value'),
+			  Input('min-lim-value', 'value'),
+			  Input('yaxis-column', 'value'),
+			  Input('upper-limit-value', 'value'),
+			  Input('max-lim-value', 'value')],
+			 [State('upload-data', 'filename'),
+			  State('upload-data', 'last_modified')])
+def update_output(contents, xName, minLim, minLimVal, yNames, maxLim, maxLimVal, filename, states):
+	if contents is not None:
+		children = parse_contents(contents, xName, minLim, minLimVal, yNames, maxLim, maxLimVal, filename, states)
+		return children
+
+
+@app.callback(Output('xaxis-column', 'options'),
+			  [Input('upload-data', 'contents')],
+			  [State('upload-data', 'filename'),])
+def get_available_indicators(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    available_indicators = list(df.columns)
+    options=[{'label': i, 'value': i} for i in available_indicators]
+    return options
+
+@app.callback(Output('yaxis-column', 'options'),
+			  [Input('upload-data', 'contents')],
+			  [State('upload-data', 'filename')])
+def get_available_indicators1(contents, filename):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            # Assume that the user uploaded a CSV file
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8')))
+        elif 'xls' in filename:
+            # Assume that the user uploaded an excel file
+            df = pd.read_excel(io.BytesIO(decoded))
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+    available_indicators = list(df.columns)
+    options=[{'label': i, 'value': i} for i in available_indicators]
+    return options
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
