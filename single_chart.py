a5b4834e562ba9da9097d04500f06c4cc43a5196
volvo007 <foxprowing@163.com>
2019-07-14 22:58:42 +0800

    Could change chart type

--- single_chart.py
+++ single_chart.py
@@ -39,7 +39,7 @@ cache.init_app(app.server, config=CACHE_CONFIG)
 
 app.layout = html.Div([
     html.H2('Step 1. Upload an excel or csv file.'),
-    # Upload component
+	# Upload component
     dcc.Upload(
         id='upload-data',
         children=html.Div([
@@ -56,29 +56,21 @@ app.layout = html.Div([
     ),
 
     # xaxis, need lower limitation or not, limitation value
-    html.H2('Step 2. Choose the xaxis and yaxis.'),
+    html.H2('Step 2. Choose xaxis and yaxis.'),
     html.Div([
         dcc.Dropdown(
-            id='xaxis-column',
-            # options=options,
-            value='',
-            # value='ENGINE KM'
-            placeholder='Select a feature as xaxis...'
+            id='xaxis-column', value='', placeholder='Select a feature as x-axis...'
         ),
         html.Div([
             html.H5('Need Lower-limitation?'),
             dcc.RadioItems(
                 id='min-limit-value',
                 options=[{'label': i, 'value': i} for i in ['Lower-Limit', 'No Need']],
-                value='No Need',
-                labelStyle={'display': 'inline-block'}
+                value='No Need', labelStyle={'display': 'inline-block'}
         ),
             dcc.Input(
-                id='min-lim-value',
-                placeholder='Enter a value...',
-                type='text',
-                value=''
-        ),
+                id='min-lim-value', placeholder='Enter a value...', type='number', value=''
+		),
         ],
             style={'border': '2px dashed #999', 'borderRadius': '5px', 
                     'padding': '10px', 'margin': '5px 5px'}
@@ -89,50 +81,48 @@ app.layout = html.Div([
     # yaxis, need upper limitation or not, limitation value
     html.Div([
         dcc.Dropdown(
-            id='yaxis-column',
-            # options=options,
-            value='',
-            # value='Iron ppm FE',
-            placeholder='Select features as yaxis...',
-            multi=True
+            id='yaxis-column', value='', placeholder='Select features as y-axis...', multi=True
         ),
         html.Div([
             html.H5('Need Upper-limitation?'),
             dcc.RadioItems(
                 id='upper-limit-value',
                 options=[{'label': i, 'value': i} for i in ['Upper-Limit', 'No Need']],
-                value='No Need',
-                labelStyle={'display': 'inline-block'}
+                value='No Need', labelStyle={'display': 'inline-block'}
             ),
             dcc.Input(
-                id='max-lim-value',
-                placeholder='Enter a value...',
-                type='text',
-                value=''
-            )],
+                id='max-lim-value', placeholder='Enter a value...', type='number', value=''
+		    )],
             style={
-                'border': '2px dashed #999', 'borderRadius': '5px', 
-                'padding': '10px', 'margin': '5px 5px'}),
+                'border': '2px dashed #999', 'borderRadius': '5px', 'padding': '10px', 'margin': '5px 5px'}),
             ],
     style={'width': '48%', 'display': 'inline-block'}),
 
     html.Div([
-        html.H5('Choose the type of the chart: scatter or line?'),
+        html.H2('Step 3. Adjust chart properties if you need'),
+        html.Div(['Adjust chart properties such as chart type or add y-axis title, chart title, etc.']),
+        dcc.Input(
+            id='chart-title', placeholder='customize chart title', type='text', value='',
+            style={'width': '40%', 'display': 'inline-block', 'margin': '5px 5px'}
+        ),
+        dcc.Input(
+            id='y-axis-title', placeholder='customize y-axis title', type='text', value='',
+            style={'width': '40%', 'display': 'inline-block', 'margin': '5px 5px'}
+        ),
         dcc.Dropdown(
-            id='scatter-mode',
-            value='markers',
+            id='scatter-mode', value='markers',
             options=[{'label': i, 'value': j} for i, j in 
-                        zip(['Marker', 'Line', 'Marker+Line'], ['markers', 'lines', 'lines+markers'])]
-        )
-    ],
-        style={'width': '48%'}),
+                        zip(['Marker', 'Line', 'Marker+Line'], ['markers', 'lines', 'lines+markers'])],
+            style={'width': '30%', 'margin': '0px 5px'}
+        ),
+    ]),
 
     html.Div(id='output-data-upload'),
 ])
 
 # get useful data from dataset
-def parse_contents(contents, xName, minLim, minLimVal, yNames, 
-                    maxLim, maxLimVal, marker_mode, filename, states):
+def parse_contents(contents, xName, minLim, minLimVal, yNames, maxLim, 
+                    maxLimVal, chartTitle, yAxisName, marker_mode, filename, states):
     content_type, content_string = contents.split(',')
     decoded = base64.b64decode(content_string)
     try:
@@ -158,45 +148,42 @@ def parse_contents(contents, xName, minLim, minLimVal, yNames,
     else:
         marker = {'size': 7, 'opacity': 0.65}
     for i in yNames:
-        traces.append(go.Scatter(
-                                x=df.loc[:, xName],
-                                y=df.loc[:, i],
-                                mode=marker_mode,
-                                marker=marker,
+    	traces.append(go.Scatter(
+    							x=df.loc[:, xName], y=df.loc[:, i], mode=marker_mode, marker=marker,
                                 opacity=0.7,
-                                # marker={'size': 10, 'opacity': 0.65, 'line': {'width': 0.5, 'color': 'white'}},
-                                name=i,
-            ))
+    							# marker={'size': 10, 'opacity': 0.65, 'line': {'width': 0.5, 'color': 'white'}},
+    							name=i,
+    		))
 
     # 根据 minLim等的情况，决定是否加线
     # plt.vlines(0, 0, 0.5, colors = "c", linestyles = "dashed")
     # plotly 里面加线：https://plot.ly/python/shapes/
     if (minLim == 'No Need') & (maxLim == 'No Need'):
-            layout = go.Layout(xaxis={'title': xName},
-                        margin={'l': 40, 'r':20, 't':20, 'b':30},
-                        hovermode='closest')
+    	    layout = go.Layout(xaxis={'title': xName}, yaxis={'title': yAxisName}, 
+                        title=dict(text=chartTitle, font={'size': 20}), margin={'l': 50, 'r':20, 't':50, 'b':50},
+    					width=1480, height=800, hovermode='closest')
     if (minLim != 'No Need') & (maxLim == 'No Need'):
-            layout = go.Layout(xaxis={'title': xName},
-                        margin={'l': 40, 'r':20, 't':20, 'b':30},
-                        hovermode='closest',
+    	    layout = go.Layout(xaxis={'title': xName}, yaxis={'title': yAxisName}, 
+                        title=dict(text=chartTitle, font={'size': 20}), margin={'l': 50, 'r':20, 't':50, 'b':50},
+    					width=1480, height=800, hovermode='closest',
                         shapes=[
                             {'type': 'line', 'x0': min(df[xName])*0.9, 'y0': minLimVal, 
                             'x1': max(df[xName])*1.1, 'y1': minLimVal,
                             'line': {'color': 'rgb(240, 48, 48)', 'width':2, 'dash': 'dashdot'}}
                         ])
     if (minLim == 'No Need') & (maxLim != 'No Need'):
-            layout = go.Layout(xaxis={'title': xName},
-                        margin={'l': 40, 'r':20, 't':20, 'b':30},
-                        hovermode='closest',
+    	    layout = go.Layout(xaxis={'title': xName}, yaxis={'title': yAxisName},
+                        title=dict(text=chartTitle, font={'size': 20}), margin={'l': 50, 'r':20, 't':50, 'b':50},
+    					width=1480, height=800, hovermode='closest',
                         shapes=[
                             {'type': 'line', 'x0': min(df[xName])*0.9, 'y0': maxLimVal, 
                             'x1': max(df[xName])*1.1, 'y1': maxLimVal,
                             'line': {'color': 'rgb(240, 48, 48)', 'width':2, 'dash': 'dashdot'}}
                         ])
     if (minLim != 'No Need') & (maxLim != 'No Need'):
-            layout = go.Layout(xaxis={'title': xName},
-                        margin={'l': 40, 'r':20, 't':20, 'b':30},
-                        hovermode='closest',
+    	    layout = go.Layout(xaxis={'title': xName}, yaxis={'title': yAxisName}, 
+                        title=dict(text=chartTitle, font={'size': 20}), margin={'l': 50, 'r':20, 't':50, 'b':50},
+    					width=1480, height=800, hovermode='closest',
                         shapes=[
                             {'type': 'line', 'x0': min(df[xName])*0.9, 'y0': maxLimVal, 
                             'x1': max(df[xName])*1.1, 'y1': maxLimVal,
@@ -210,31 +197,31 @@ def parse_contents(contents, xName, minLim, minLimVal, yNames,
     return html.Div([
         html.H5('Filename: {}'.format(filename)),
         html.H5('Last modified: {}'.format(str(datetime.datetime.fromtimestamp(states))[:-7])),
-        dcc.Graph(
-            figure={
-             'data': traces,
-             'layout': layout,}
-            )
+	    dcc.Graph(
+	    	figure={
+	         'data': traces,
+	         'layout': layout,}
+	    	)
     ])
 
 @app.callback(Output('output-data-upload', 'children'),
-             [Input('upload-data', 'contents'), Input('xaxis-column', 'value'),
-              Input('min-limit-value', 'value'), Input('min-lim-value', 'value'),
-              Input('yaxis-column', 'value'), Input('upper-limit-value', 'value'),
-              Input('max-lim-value', 'value'), Input('scatter-mode', 'value')],
-             [State('upload-data', 'filename'),
-              State('upload-data', 'last_modified')])
-def update_output(contents, xName, minLim, minLimVal, yNames, 
-                    maxLim, maxLimVal, marker_mode, filename, states):
-    if contents is not None:
-        children = parse_contents(contents, xName, minLim, minLimVal, yNames, 
-                                    maxLim, maxLimVal, marker_mode, filename, states)
-        return children
+			 [Input('upload-data', 'contents'), Input('xaxis-column', 'value'),
+			  Input('min-limit-value', 'value'), Input('min-lim-value', 'value'),
+			  Input('yaxis-column', 'value'), Input('upper-limit-value', 'value'),
+			  Input('max-lim-value', 'value'), Input('chart-title', 'value'), 
+              Input('y-axis-title', 'value'),Input('scatter-mode', 'value')],
+			 [State('upload-data', 'filename'), State('upload-data', 'last_modified')])
+def update_output(contents, xName, minLim, minLimVal, yNames, maxLim, 
+                    maxLimVal, chartTitle, yAxisName, marker_mode, filename, states):
+	if contents is not None:
+		children = parse_contents(contents, xName, minLim, minLimVal, yNames, maxLim, 
+                    maxLimVal, chartTitle, yAxisName, marker_mode, filename, states)
+		return children
 
 
 @app.callback(Output('xaxis-column', 'options'),
-              [Input('upload-data', 'contents')],
-              [State('upload-data', 'filename'),])
+			  [Input('upload-data', 'contents')],
+			  [State('upload-data', 'filename'),])
 def get_available_indicators(contents, filename):
     if contents is not None:
         content_type, content_string = contents.split(',')
@@ -259,8 +246,8 @@ def get_available_indicators(contents, filename):
         return ''
 
 @app.callback(Output('yaxis-column', 'options'),
-              [Input('upload-data', 'contents')],
-              [State('upload-data', 'filename')])
+			  [Input('upload-data', 'contents')],
+			  [State('upload-data', 'filename')])
 def get_available_indicators1(contents, filename):
     if contents is not None:
         content_type, content_string = contents.split(',')
@@ -285,4 +272,4 @@ def get_available_indicators1(contents, filename):
         return ''
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='192.168.1.18', port=8050)
